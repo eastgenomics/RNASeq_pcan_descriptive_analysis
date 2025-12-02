75c68b694983e0c147731636a9d84a83659dd027
Noepiga <noemi.piga@nhs.net>
2025-12-02 17:19:33 +0000

    add comment line

--- src/genefusion_technique_comparison.sh
+++ src/genefusion_technique_comparison.sh
@@ -5,12 +5,12 @@ mkdir input
 
 #NHS Test Directory gene fusion (from DNA Nexus):
 #file ID: file-GpGVQZj4JJKy6vGyZz2GQ2p2
-cd input/
 dx download file-GpGVQZj4JJKy6vGyZz2GQ2p2
 
 #TruSight gene fusion list:
+cd input/
 wget -P input/ https://emea.illumina.com/content/dam/illumina-marketing/documents/products/gene_lists/gene_list_trusight_pan_cancer.xlsx
-
+#Convert xlsx into a text file in python:
 
 #How many gene fusions in NHS Test Directory can be detected by the TruSight gene fusion kit ?
 #Script in python to compare - need to read the xlsx file.
