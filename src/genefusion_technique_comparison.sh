4236867f587a3c088709e663be583d6b81d16d03
Noepiga <noemi.piga@nhs.net>
2025-12-05 17:37:40 +0000

    compare trusight with NHS test directory gene fusions list

--- src/genefusion_technique_comparison.sh
+++ src/genefusion_technique_comparison.sh
@@ -1,16 +1,36 @@
 #!/bin/usr/sh
 
 #Rationale: compare gene fusions detectable from PANCAN and RES with the gene fusion reported in the NHS Test Directory
-mkdir input
 
 #NHS Test Directory gene fusion (from DNA Nexus):
 #file ID: file-GpGVQZj4JJKy6vGyZz2GQ2p2
-dx download file-GpGVQZj4JJKy6vGyZz2GQ2p2
 
 #TruSight gene fusion list:
-cd input/
-wget -P input/ https://emea.illumina.com/content/dam/illumina-marketing/documents/products/gene_lists/gene_list_trusight_pan_cancer.xlsx
-#Convert xlsx into a text file in python:
+wget -P input/ -N https://emea.illumina.com/content/dam/illumina-marketing/documents/products/gene_lists/gene_list_trusight_pan_cancer.xlsx
+
+##Convert xlsx into a text file in python:
+python3 src/convert_xlsx_into_tsv.py \
+    input/gene_list_trusight_pan_cancer.xlsx \
+    input/formatted_gene_list_trusight_pan_cancer.tsv
 
 #How many gene fusions in NHS Test Directory can be detected by the TruSight gene fusion kit ?
-#Script in python to compare - need to read the xlsx file.
+LEN_TEST_DIRECTORY_FUSION=$(dx cat file-GpGVQZj4JJKy6vGyZz2GQ2p2 | wc -l)
+echo "Test Directory contains ${LEN_TEST_DIRECTORY_FUSION} gene fusions to test"
+
+TEST_DIRECTORY_IN_TRUSIGHT=$(dx cat file-GpGVQZj4JJKy6vGyZz2GQ2p2 | \
+    grep -F -f input/formatted_gene_list_trusight_pan_cancer.tsv -w -i | \
+    wc -l)
+echo "TruSight recognises ${TEST_DIRECTORY_IN_TRUSIGHT} gene fusion of the NHS Test Directory"
+echo "Saving the recognised gene funsions into DNANexus project 003_RNA_251118_RNASeq_GeneExpression"
+dx cat file-GpGVQZj4JJKy6vGyZz2GQ2p2 | \
+    grep -F -f input/formatted_gene_list_trusight_pan_cancer.tsv -w -i --color=always | \
+    dx upload - --path input/trusight_in_testdirectory.tsv
+
+TEST_DIRECTORY_NOTIN_TRUSIGHT=$(dx cat file-GpGVQZj4JJKy6vGyZz2GQ2p2 | \
+    grep -F -f input/formatted_gene_list_trusight_pan_cancer.tsv -w -i -v | \
+    wc -l)
+echo "TruSight does not recognise ${TEST_DIRECTORY_NOTIN_TRUSIGHT} gene fusion of the NHS Test Directory"
+echo "Saving the NON recognised gene fusions into DNANexus project 003_RNA_251118_RNASeq_GeneExpression"
+dx cat file-GpGVQZj4JJKy6vGyZz2GQ2p2 | \
+    grep -F -f input/formatted_gene_list_trusight_pan_cancer.tsv -w -i -v | \
+    dx upload - --path input/trusight_notin_testdirectory.tsv
