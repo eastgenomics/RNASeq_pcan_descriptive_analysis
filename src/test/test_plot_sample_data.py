26e38e1941cccc09eb3fd82b4dc4120a8986b3df
Noepiga <noemi.piga@nhs.net>
2026-01-07 11:24:03 +0000

    change type of error (system to filenotfound)

--- src/test/test_plot_sample_data.py
+++ src/test/test_plot_sample_data.py
@@ -1,12 +1,12 @@
 import pandas as pd
 import pytest
 
-import pcan_samples_descriptive as psd
+import plot_sample_data as psd
 
 def test_that_error_raised_if_not_found_file():
     """
-    Test that a SystemError is raised if an input file is not found
+    Test that a FileNotFoundError is raised if an input file is not found
     """
     input_file = "input/not_existing_file.xlsx"
-    with pytest.raises(SystemError):
+    with pytest.raises(FileNotFoundError):
         psd.read_txt_to_dataframe(input_file)
